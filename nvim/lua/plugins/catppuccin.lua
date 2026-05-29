e121a0e1859f9ecde24536857708563adc7a7d55
dewei.ma <xnmageis@outlook.com>
2026-05-29 17:15:42 +0800

    feat(nvim): 配置 catppuccin 主题和颜色方案
    添加 catppuccin 主题初始化配置，设置 mocha 深色主题和相关插件集成

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,4 +2,19 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      background = { light = "latte", dark = "mocha" },
+      transparent_background = false,
+      integrations = {
+        notify = true,
+        nvimtree = true,
+        mini = true,
+        dap = true,
+        native_lsp = { enabled = true },
+      },
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
